4eb06940f5596e50d9131897d662688e26e5881a
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2023-09-20 10:14:23 +0200

    a

--- File/Pruebas/JavaPrueba/src/javaprueba/JavaPrueba.java
+++ File/Pruebas/JavaPrueba/src/javaprueba/JavaPrueba.java
@@ -2,27 +2,49 @@ package javaprueba;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 
 public class JavaPrueba {
     public static void main(String[] args) {
         
-        File f = new File("a.txt");
+        
+        // Forma 1
+        /*File f;
+         
+        if(args.length < 1){
+            System.out.print("Falta nombre en argumentos.\n");
+            return;
+        } else {
+            String nomFich = args[0];
+            f = new File(nomFich);
+            try {
+            f.createNewFile();
+        } catch (IOException ex) {
+            System.err.printf("ERROR: %s", ex.getMessage());
+        }
+        }*/
+        
+        // Forma 2
+        if(args.length < 1){
+            System.out.print("Falta nombre en argumentos.\n");
+            return;
+        }
+        String nomFich = args[0];
+        File f = new File(nomFich);
         
         try {
             f.createNewFile();
         } catch (IOException ex) {
-            Logger.getLogger(JavaPrueba.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.printf("ERROR: %s", ex.getMessage());
         }
         
         if(f.exists()){
-            System.out.printf("%s existe.\n", f.getName());
+            //System.out.printf("%s existe.\n", f.getName()); - Profesor le gusta esta forma
+            System.out.println(f.getName() + " existe.");
         } else {
             System.out.printf("%s no existe.\n", f.getName()); 
         }
         
+        //File.separator; Para separar segun el sistema con / o \
     }
     
 }
