251e199849d3ad3c2997b43cbb6c6093de543d42
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2023-10-03 14:27:21 +0200

    a

---
+++ Tema 1/Ejercicios de repaso/Ejercicio1punto14/src/ejercicio1punto14/Ejercicio1punto14.java
@@ -0,0 +1,24 @@
+package ejercicio1punto14;
+
+/**
+ * Todo lo que se pide en esta actividad debe estar en un mismo proyecto. Crea
+ * una clase Empleado, con atributos numEmp de tipo int, dni de tipo String,
+ * nombre de tipo String, salBrutoAnual de tipo double, y tParcial de tipo
+ * boolean. Crea un programa que cree tres objetos de la clase Empleado y
+ * escriba sus datos en un fichero, utilizando la clase DataOutputStream. Debes
+ * crear una nueva clase para ello, con un método main para que sea ejecutable.
+ * Crea un programa que lea un fichero con datos de empleados, utilizando la
+ * clase DataInputStream, y que a partir de ellos cree los correspondientes
+ * objetos de la clase Empleado, y que los introduzca en una lista (objeto de
+ * una clase cualquiera que implemente la interfaz List), y después itere sobre
+ * la lista para mostrar los datos de cada empleado. Debes crear una nueva clase
+ * para ello, con un método main para que sea ejecutable.
+ *
+ */
+public class Ejercicio1punto14 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
